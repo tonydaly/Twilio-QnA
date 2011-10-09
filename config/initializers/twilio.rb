48354a8760857a7a58d8c16dbcf2ea9ccce20956
Tony Daly <tony.daly@me.com>
2011-10-09 12:30:57 +0100

    Use environment variables

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,3 @@
-twilio_config = YAML.load_file("#{Rails.root}/config/twilio.yml")[Rails.env]
-
 Twilio::Config.setup \
   :account_sid  => ENV["TWILIO_ACCOUNT_SID"],
   :auth_token   => ENV["TWILIO_AUTH_TOKEN"]
